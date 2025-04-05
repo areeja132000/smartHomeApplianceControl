e1ee1cc649d4b2a13cffab903fd7148e898d0e84
areeja132000 <57684414+areeja132000@users.noreply.github.com>
2025-04-05 16:18:29 -0400

    Update SmartHomeControl.java
    spacing

--- src/smartHomeApplianceControl/SmartHomeControl.java
+++ src/smartHomeApplianceControl/SmartHomeControl.java
@@ -11,28 +11,28 @@ public class SmartHomeControl {
 	
 	public SmartHomeControl () {
 		this.appliances = new ArrayList<>();
-		
-        //At creation, setting it up so that system will turn off all devices on January 1st 1:00am
-        Timer timer = new Timer(false);
-        
-        LocalDateTime current = LocalDateTime.now();
-        int targetYear = current.getYear();
-        
-        LocalDateTime targetDate = LocalDateTime.of(targetYear, 1, 1, 1, 0, 0, 0);
-
-        // If the current date is after Jan 1st, 1:00 AM, need to use next year
-        if (current.isAfter(targetDate)) {
-        	targetYear++;
-        }
-        
-        Duration duration = Duration.between(current, LocalDateTime.of(targetYear, 1, 1, 1, 0, 0, 0));
-        
-        //Delay is time between now and next Jan 1st, 1:00 AM
-        long delay = duration.toMillis();
-        //Repeats every year
-        long period = 1000L * 60L * 60L * 24L * 365L;
-
-        timer.scheduleAtFixedRate(new MyTimerTask(this), delay, period);
+			
+	        //At creation, setting it up so that system will turn off all devices on January 1st 1:00am
+	        Timer timer = new Timer(false);
+	        
+	        LocalDateTime current = LocalDateTime.now();
+	        int targetYear = current.getYear();
+	        
+	        LocalDateTime targetDate = LocalDateTime.of(targetYear, 1, 1, 1, 0, 0, 0);
+	
+	        // If the current date is after Jan 1st, 1:00 AM, need to use next year
+	        if (current.isAfter(targetDate)) {
+	        	targetYear++;
+	        }
+	        
+	        Duration duration = Duration.between(current, LocalDateTime.of(targetYear, 1, 1, 1, 0, 0, 0));
+	        
+	        //Delay is time between now and next Jan 1st, 1:00 AM
+	        long delay = duration.toMillis();
+	        //Repeats every year
+	        long period = 1000L * 60L * 60L * 24L * 365L;
+	
+	        timer.scheduleAtFixedRate(new MyTimerTask(this), delay, period);
 	}
 	
 	public void addAppliance(Appliance appliance) {
